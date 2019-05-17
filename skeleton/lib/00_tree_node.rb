caf3aa967efaea25f6435c05dadc56e9acabec10
Matt DeShaw <mwdeshaw@gmail.com>
2019-05-17 16:15:25 -0700

    Pass all specs for polytree class

--- skeleton/lib/00_tree_node.rb
+++ skeleton/lib/00_tree_node.rb
@@ -26,5 +26,31 @@ class PolyTreeNode
       child_node.parent = nil
     end 
   end
-  
+
+  def dfs(target_value)
+    return self if self.value == target_value
+
+    self.children.each do |child|
+      current = child.dfs(target_value)
+      return current unless current.nil?
+    end
+
+    nil
+  end
+
+  def bfs(target_value)
+    q = [self]
+
+    until q.empty?
+      current = q.shift
+      if current.value == target_value
+        return current
+      else
+        q += current.children
+      end
+    end
+
+    nil
+  end
+
 end
